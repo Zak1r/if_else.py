41bd4d26f1bb14fa6f3abba89997e5459d427c62
Zokirzhon Ikramzhanov <zokirzhon99@gmail.com>
2021-11-19 00:57:08 +0600

    finished hw to If-else

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 # work with if - else
+
+
 # task 1
 a = int(input('Enter your number: '))
 if a == 0:
@@ -6,5 +8,63 @@ if a == 0:
 else:
     print('Incorrect!')
 
-# task 2
 
+# task 2
+a = int(input('Enter your number: '))
+if a > 0:
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+
+# task 3
+a = int(input('Enter your number: '))
+if a < 0:
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+
+#task 4
+a = int(input('Enter your number: '))
+if a >= 0:
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+
+#task 5
+a = int(input('Enter your number: '))
+if a <= 0:
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+
+#task 6
+a = int(input('Enter your number: '))
+if  a != 0:
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+#task 7
+a = int(input('Enter something: '))
+if  a == 'test':
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+
+#task 8
+a = int(input('Enter your number: '))
+if  a == 1 and a == '1':
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+
+#task 9
+num = int(input('Enter your number: '))
+if num % 2 ==0:     print("Even number")
+else:   print('Odd number')
